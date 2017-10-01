d98b01070107c06f0e169a525eff22395e41a135
songwei3 <songwei3@illinois.edu>
2017-10-01 00:20:21 -0500

    Update FailureDetect.java

--- src/main/java/CS425/FailureDetect.java
+++ src/main/java/CS425/FailureDetect.java
@@ -1,4 +1,17 @@
 package CS425;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
 public class FailureDetect {
+    public static Logger logger = Logger.getLogger(FailureDetect.class.getName());
+
+
+    public void FailureDetect() throws IOException{
+        // TODO
+        // If both the successors and predecessor don't receive the heartbeats,
+        //which means the lastActiveTime in those membership list minus current time >3s
+        //then turn the isActive into false
+    }
+
 }
