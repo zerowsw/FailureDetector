c0bf72f9247444642b49fa9569562e9bd1e60663
songwei3 <songwei3@illinois.edu>
2017-10-01 00:22:37 -0500

    Update SendThread.java

--- src/main/java/CS425/SendThread.java
+++ src/main/java/CS425/SendThread.java
@@ -1,10 +1,33 @@
 package CS425;
 
+import java.io.IOException;
+import java.net.InetAddress;
 import java.util.logging.Logger;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 
-public class SendingThread extends Thread {
-    public static Logger logger = Logger.getLogger(SendingThread.class.getName());
-    public  SendingThread(int port){
+/***
+ * This class is used to send messages to introducer, successor, predecessor
+ */
 
+public class SendThread extends Thread {
+    public static Logger logger = Logger.getLogger(SendThread.class.getName());
+    private static String machineId;
+    private static int sendPort;
+    private static String heartbeat;
+    public SendThread(String machineId,int sendPort)throws IOException {
+        byte[] sendBuf = new byte[1024];
+        InetAddress sendAddress=null;//TODO
+        DatagramSocket ds = new DatagramSocket(8088);
+        DatagramPacket dp_send= new DatagramPacket(heartbeat.getBytes(),heartbeat.length(),sendAddress,sendPort);
+        ds.send(dp_send);
     }
+    public SendThread(String machineId,int sendPort, int location)throws IOException {
+        byte[] sendBuf = new byte[1024];
+        InetAddress sendAddress=null;//TODO
+        DatagramSocket ds = new DatagramSocket(8088);
+        DatagramPacket dp_send= new DatagramPacket(heartbeat.getBytes(),heartbeat.length(),sendAddress,sendPort);
+        ds.send(dp_send);
+    }
+
 }
