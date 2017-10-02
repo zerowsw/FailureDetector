dcad221d91398d1032599989432219abc71890c0
songwei3 <songwei3@illinois.edu>
2017-10-01 22:10:36 -0500

    Update FailureDetect.java

--- src/main/java/CS425/FailureDetect.java
+++ src/main/java/CS425/FailureDetect.java
@@ -2,16 +2,33 @@ package CS425;
 
 import java.io.IOException;
 import java.util.logging.Logger;
+import java.util.Map.Entry;
 
-public class FailureDetect {
+public class FailureDetect extends Thread {
     public static Logger logger = Logger.getLogger(FailureDetect.class.getName());
+    public static int failTime = 2000;
+    public static long currentTime = System.currentTimeMillis();
 
-
-    public void FailureDetect() throws IOException{
-        // TODO
+    public FailureDetect(String memberId) throws IOException{
         // If both the successors and predecessor don't receive the heartbeats,
-        //which means the lastActiveTime in those membership list minus current time >3s
+        //which means the lastActiveTime in those membership list minus current time >2000
         //then turn the isActive into false
+        for(Entry<String, MemberInfo> entry : MemberGroup.membershipList.entrySet())
+        {
+            MemberInfo list = entry.getValue();
+
+            // set the member which is inactive for a failTime unit to inactive
+            if(entry.getKey()!= memberId)
+            {
+                if(list.getIsActive().equalsIgnoreCase("YES")& ((currentTime- list.getActiveTime())>failTime))
+                {
+                    list.setActive(false);
+                    logger.info("The member "+entry.getKey()+"has failed");
+                }
+            }
+        }
+
+
     }
 
 }
